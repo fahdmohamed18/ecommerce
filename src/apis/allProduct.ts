f9323037d513561ed4909ccb3b97ddb28a1f4fc0
Fahd Mohamed <fahdelkholy2004@gmail.com>
2025-09-21 21:50:53 +0300

    update file

--- src/apis/allProduct.ts
+++ src/apis/allProduct.ts
@@ -1,7 +1,8 @@
 import { Product } from "@/types/product.type";
 
 export default async function getAllProducts() {
-    const response = await fetch("http://localhost:3000/api/users");
+    const base = process.env.NEXT_PUBLIC_API || "https://ecommerce.routemisr.com/api/v1";
+    const response = await fetch(`${base}/products`);
     const { data }: { data: Product[] } = await response.json();
     return data;
 }
